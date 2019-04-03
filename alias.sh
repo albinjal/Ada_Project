08da9a3a5a1d78d2e44e4df334c849645ee58316
davho299 <davho299@roxen2-109.ad.liu.se>
2019-04-03 18:13:02 +0200

    Added alias script, run with source alias.sh

---
+++ alias.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# alias for compiling and running
+# echo "What is your hostname?"
+alias gk="gnatmake $(/courses/TDDD11/TJa/bin/tja_config) klient.adb"
+alias gs="gnatmake $(/courses/TDDD11/TJa/bin/tja_config) server.adb"
+alias rk="./klient $hostname 1337"
+alias rs="./server 1337"

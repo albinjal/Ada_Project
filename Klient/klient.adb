02492912025e34794303f5096b87a78d49d8ae21
albja664 <albja664@hunn2-112.ad.liu.se>
2019-04-15 17:16:16 +0200

    bugfix

--- Klient/klient.adb
+++ Klient/klient.adb
@@ -17,23 +17,23 @@ procedure Klient is
    Continue, Switches: Integer;
    TX : String(1..100);
    TL : Integer;
-   
+
 begin
    for I in 1..5 loop
       Reroll(I) := 0;
    end loop;
-      
+
    --Denna rutin kontrollerar att programmet startas med två parametrar.
    --Annars kastas ett fel.
    --Argumentet skall vara serverns adress och portnummer, t.ex.:
    --> klient localhost 3400
-   
+
    if Argument_Count /= 2 then
       Raise_Exception(Constraint_Error'Identity,
                       "Usage: " & Command_Name & " remotehost remoteport");
    end if;
 
-   
+
    Bootup(Socket, Argument(1), Positive'Value(Argument(2)));
    Start_Game(Socket, Player);
    Get_Rolls(Socket, Roll);
@@ -45,15 +45,15 @@ begin
 	 elsif GetI(Roll) = 7 then
 	    Put("Spelare ");  Put(3-Player,1); Put(" har slagit");
 	    exit;
-	    
+
 	 end if;
       end loop;
-      
+
    else
       Put("Din tur"); New_Line;
       for I in 1..3 loop
 	 Result := GetR(Roll);
-	 
+
 	 Put("Tryck enter för att slå...");
 	 Skip_Line;
 	 Playerroll(Socket);
@@ -71,34 +71,33 @@ begin
 	      Get(B);
 	      Reroll(B) := 1;
 	   end loop;
-	   
+
 	   Put("STAT1"); New_Line;
 
-	   
-	   Put(Socket,'6'); Put(Socket,Switches);
-	   
+
+	   Put(Socket,'6'); Put(Socket,Switches,0);
+
 	   Put("STAT2"); New_Line;
-	   
+
 	   for A in 1..5 loop
-	      Put(Socket,Reroll(A));
+	      Put(Socket,Reroll(A),0);
 	   end loop;
-	   
+
 	   Put("STAT3"); New_Line;
-	   
+
 	   New_Line(Socket);
 	   Get_Rolls(Socket, Roll);
-	   
-	   
-      end loop;
-      
-   end if;
-      
 
-   
-  
-   
-   
-   
+
+      end loop;
+
+   end if;
+
+
+
+
+
+   Skip_Line;
    --Innan programmet avslutar stängs socketen, detta genererar ett exception
    --hos servern, pss kommer denna klient få ett exception när servern avslutas
    Close(Socket);
--- Server/server_assets_package.adb
+++ Server/server_assets_package.adb
@@ -2,12 +2,12 @@ with Ada.Exceptions;      use Ada.Exceptions;
 with Ada.Text_IO;         use Ada.Text_IO;
 with Ada.Numerics.Discrete_Random;
 with TJa.Sockets;         use TJa.Sockets;
-
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
 package body Server_Assets_Package is
    function Generate
      return Integer is
-      
+
       subtype Nums is Integer range 1..6;
       package RN is
 	 new Ada.Numerics.Discrete_Random(Nums);
@@ -16,26 +16,26 @@ package body Server_Assets_Package is
       RN.Reset(Gen);
       return RN.Random(Gen);
    end;
-   
-   
+
+
    function Roll
      return String is
       S: String(1..5);
    begin
-      
+
       for X in 1..5 loop
-	 
+
 	 S(X) := Integer'Image(Generate)(2);
       end loop;
       return S;
    end;
-   
-   
+
+
    procedure Connect_To_Klients(Socket1, Socket2: in out Socket_Type;
 				Lyssnare : in out Listener_Type;
 				Port: in Positive) is
    begin
-      
+
     Initiate(Lyssnare, Port, Localhost => False);
    --| *** eller ***
    --| Initierar lyssnaren på en port (klienter bara på "localhost").
@@ -52,51 +52,61 @@ package body Server_Assets_Package is
    Put_Line(Socket1, "3");
    --| Nu har en anslutning skapats och vi kan då börja en loop eller något
    Put_Line("Klienter anslutna...");
-      
+
    end;
-   
+   function Read(C: in Character)
+		return Natural is
+      S: String(1..1);
+      i: Natural;
+   begin
+     Put(C);
+      S(1) := C;
+      Put(S);
+      i := Integer'Value(S);
+      Put(i,1);
+      return i;
+   end;
+
    procedure Yatzyloop(Socket1, Socket2: in out Socket_Type) is
       type Rerolls is array(1..5) of Integer;
-      function Read(C: in Character)
-		   return Natural is
-	 S: String(1..1);
-      begin
-	 S(1) := C;
-	 return Integer'Value(S);
-      end;
       Reroll: Rerolls;
       TX: String(1..100);
       TL, I: Integer;
       CurrentRolls: String(1..5);
-      
+
    begin
      CurrentRolls := Roll(1..5);
      Put(Socket1, "45"); Put(Socket1, CurrentRolls); New_Line(Socket1);
      Put_Line(Socket2,"50");
      Get_Line(Socket1, TX, TL);
-     
+
      if TX(1) = '5' then
 	if TX(2) = '1' then
 	   Put_Line(Socket2, "51");
 	end if;
      end if;
-     
      Get_Line(Socket1, TX, Tl);
      if TX(1) = '6' then
 	I := Read(TX(2));
+  Put(I);
+  -- ERROR IS HERE ^
+  Put("1");
 	for X in 3..7 loop
 	   Reroll(X-2) := Read(TX(X));
 	end loop;
+  Put("2");
 	for X in 1..5 loop
 	   if Reroll(X) = 1 then
 	      CurrentRolls(X) := Roll(1);
 	   end if;
 	end loop;
+  Put("Sending");
 	Put(Socket1, "45"); Put(Socket1, CurrentRolls); New_Line(Socket1);
+  Put("Sent");
      end if;
-     
-	   
-     
+
+
+
    end;
-   
+
 end;

b206d57cf5de29f67d333ffbe35c71aa8a94eece
loufo208 <loufo208@hunn2-112.ad.liu.se>
2019-04-15 13:39:11 +0200

    lul

--- Klient/klient_assets_package.adb
+++ Klient/klient_assets_package.adb
@@ -3,7 +3,7 @@ with Ada.Exceptions;      use Ada.Exceptions;
 with Ada.Text_IO;         use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 package body Klient_Assets_Package is
-   
+
    procedure Bootup(Socket: out Socket_Type; Adress: in String; Port: in Positive) is
    begin
       Initiate(Socket);
@@ -11,13 +11,13 @@ package body Klient_Assets_Package is
       Put("Ansluten till servern");
       New_Line;
    end;
-   
+
    procedure Start_Game(Socket: in Socket_Type; Player: out Positive) is
    TX     : String(1..100);
    TL : Natural;
    begin
       Get_Line(Socket, TX, TL);
-      
+
       if TX(1) = '1' then
 	 Put("Du är spelare 1, väntar på spelare 2");
 	 Player := 1;
@@ -26,7 +26,7 @@ package body Klient_Assets_Package is
 	 if TX(1) = '3' then
 	    Put("Båda spelare anslutna");
 	 end if;
-	 
+
       elsif TX(1) = '2' then
 	 Put("Du är spelare 2");
 	 Player := 2;
@@ -38,7 +38,7 @@ package body Klient_Assets_Package is
       New_Line;
 
    end;
-   
+
    function Read(C: in Character)
 		return Natural is
       S: String(1..1);
@@ -46,20 +46,20 @@ package body Klient_Assets_Package is
       S(1) := C;
       return Integer'Value(S);
    end;
-   
-   
+   --hej
+
    procedure Get_Rolls(Socket: in Socket_Type; Roll: out Rolls_Type) is
       TX: String(1..100);
       TL: Natural;
    begin
-      
+
       Put("STAT111"); New_Line;
       Get_Line(Socket, TX, TL);
-      
+
       New_Line;
-      
+
       Put("STAT222"); New_Line;
-      
+
       if TX(1) = '4' then
 	 -- 4 betyder inkomande tärningar
 	 Roll.I := Read(TX(2));
@@ -67,9 +67,9 @@ package body Klient_Assets_Package is
 	 for X in  1..Roll.I loop
 	    Roll.Rolls(X) := Read(TX(X+2));
 	 end loop;
-	 
+
 	 Put("STAT333"); New_Line;
-	
+
       elsif TX(1) = '5' then
 	 -- 5 betyder info om gamestate
 	 if TX(2) = '0' then
@@ -77,18 +77,18 @@ package body Klient_Assets_Package is
 	 elsif TX(2) = '1' then
 	    Roll.I := 7;
 	 end if;
-	 
+
    else
       raise DATATYPE_ERROR;
-      
-      
+
+
       end if;
    end;
-   
+
    function GetI(Roll: in Rolls_Type)
 		return Integer is
    begin
-      
+
       return Roll.I;
    end;
    function GetR(Roll: in Rolls_Type)
@@ -96,28 +96,28 @@ package body Klient_Assets_Package is
    begin
       return Roll.Rolls;
    end;
-   
+
    procedure Playerroll(Socket: in Socket_Type) is
    begin
       Put_Line(Socket, "51");
    end;
 
    procedure Sort(Arrayen_Med_Talen: in out Arr) is
-      procedure Swap(Tal_1,Tal_2: in out Integer) is 
+      procedure Swap(Tal_1,Tal_2: in out Integer) is
 	 Tal_B : Integer; -- Temporary buffer
       begin
 	 Tal_B := Tal_1;
 	 Tal_1 := Tal_2;
 	 Tal_2 := Tal_B;
-	 
-	 
+
+
       end Swap;
-      
+
 
       Minsta_Talet, Minsta_Talet_Index : Integer;
    begin
       Minsta_Talet := 0;
-      
+
       for IOuter in Arrayen_Med_Talen'Range loop
 
          for I in IOuter..Arrayen_Med_Talen'Last loop
@@ -127,33 +127,33 @@ package body Klient_Assets_Package is
 	       Minsta_Talet_Index := I;
 	    end if;
          end loop;
-	 
+
 	 Swap(Arrayen_Med_Talen(IOuter), Arrayen_Med_Talen(Minsta_Talet_Index));
 
       end loop;
    end Sort;
-   
-   
+
+
    function Calcpoints(Prot: Protocoll_Type; Rolls: Arr)
 		      return Protocoll_Type is
-      
+
       function Ental(I: Integer; Rolls: Arr)
 		    return Integer is
-	 
+
 	 C : Integer := 0;
-	 
+
       begin
 	 for X in 1..5 loop
 	    if Rolls(X) = I then
 	       C := C + I;
 	    end if;
 	 end loop;
-	 return C;	    
+	 return C;
       end;
-      
+
             function FourPair(Rolls: Arr)
 		       return Integer is
-	 
+
       begin
 	 for I in 1..2 loop
 	       if Rolls(I) = Rolls(I+1) and Rolls(I) = Rolls(I+2) and Rolls(I) = Rolls(I+3) then
@@ -172,14 +172,14 @@ package body Klient_Assets_Package is
 	 end loop;
 	 return 0;
       end;
-      
+
       function TwoPair(Rolls: Arr)
 		      return Integer is
       begin
 	 if FourPair(Rolls) /= 0 then
 	    return 0;
 	 end if;
-	 
+
 	 for I in 1..2 loop
 	    if Rolls(I) = Rolls(I+1) then
 	       for X in (I+2)..4 loop
@@ -194,7 +194,7 @@ package body Klient_Assets_Package is
       end;
       function Triss(Rolls: Arr)
 		    return Integer is
-	 
+
       begin
 	 for I in 1..3 loop
 	    if Rolls(I) = Rolls(I+1) and Rolls(I) = Rolls(I+2) then
@@ -202,7 +202,7 @@ package body Klient_Assets_Package is
 	    end if;
 	 end loop;
 	 return 0;
-	 
+
       end;
       function Stege(I: Integer; Rolls: Arr)
 		    return Integer is
@@ -212,7 +212,7 @@ package body Klient_Assets_Package is
 	    when 11 =>
 	       --Liten
 	       for X in  1..5 loop
-		  
+
 		  if Rolls(6-X) /= X then
 		     return 0;
 		  end if;
@@ -220,7 +220,7 @@ package body Klient_Assets_Package is
 	       return 15;
 	    when 12 =>
 	       --Stor
-	       for X in 2..6 loop 
+	       for X in 2..6 loop
 		  if Rolls(7-X) /= X then
 		     return 0;
 		  end if;
@@ -230,9 +230,9 @@ package body Klient_Assets_Package is
 	      null;
 	       return 0;
 	 end case;
-	 
+
       end;
-      
+
       function Chans(Rolls: Arr)
 		    return Integer is
 	 X : Integer := 0;
@@ -242,13 +242,13 @@ package body Klient_Assets_Package is
 	 end loop;
 	 return X;
       end;
-      
+
       function Kaok(Rolls: Arr)
 		   return Integer is
       begin
 	 for X in 1..2 loop
 	    exit when Rolls(X) /= Rolls(X+1);
-	    
+
 	    if X = 2 then
 	       if Rolls(4) = Rolls(5) and Rolls(5) /= Rolls(1) then
 		  return Chans(Rolls);
@@ -256,18 +256,18 @@ package body Klient_Assets_Package is
 		  return 0;
 	       end if;
 	    end if;
-	    
+
 	 end loop;
-	 
+
 	 if Rolls(1) = Rolls(2) then
 	    if Rolls(3) = Rolls(4) and Rolls(3) = Rolls(5) then
 	       return Chans(Rolls);
 	    end if;
 	 end if;
-	
+
 	 return 0;
       end;
-      
+
       function Yatzy(Rolls: Arr)
 	   return Integer is
       begin
@@ -278,19 +278,19 @@ package body Klient_Assets_Package is
 	 end loop;
 	 return 50;
       end;
-      
-      
-      
-      
-      
-      
-      
+
+
+
+
+
+
+
       TRolls: Arr := Rolls;
       Result: Protocoll_Type;
    begin
       Sort(TRolls);
-     
-      
+
+
       for I in 1..15 loop
 	 if Prot(I) /= -1 then
 	    Result(I) := -1;
@@ -317,17 +317,17 @@ package body Klient_Assets_Package is
 	       when others =>
 		  Result(I) := 0;
 	    end case;
-	    
-		    
-		  
+
+
+
 	 end if;
       end loop;
-	 
+
       return Result;
    end;
-     
 
-   
-      
-      
+
+
+
+
 end Klient_Assets_Package;
